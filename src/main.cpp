9bad5e43ea646fd5cd752f757ae43223e32b85c2
Martin Beľa <martin.bela.sk@gmail.com>
2021-07-26 15:25:43 +0200

    Changed tabs to 8 spaces

--- src/main.cpp
+++ src/main.cpp
@@ -10,127 +10,122 @@
 
 using namespace std::literals;
 namespace {
-	using c_str = const char*;
+        using c_str = const char*;
 
-	template<typename fun>
-	class scope_exit {
-		fun function;
-	public:
-		scope_exit(fun function) :
-			function{ std::move(function) } { }
-		~scope_exit() {
-			function();
-		}
-	};
+        template<typename fun>
+        class scope_exit {
+                fun function;
+        public:
+                scope_exit(fun function) :
+                        function{ std::move(function) } { }
+                ~scope_exit() {
+                        function();
+                }
+        };
 
+        void glfw_error_callback(int error, const char* description) {
+                std::cout<< "Error: " << description << '\n';
+        }
 
-	void glfw_error_callback(int error, const char* description) {
-		std::cout<< "Error: " << description << '\n';
-	}
+        void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+                if ((key == GLFW_KEY_ESCAPE || key == GLFW_KEY_Q) && action == GLFW_PRESS) {
+                        glfwSetWindowShouldClose(window, GLFW_TRUE);
+                }
+        }
 
+        void window_resize_callback(GLFWwindow* window, int width, int height);
 
-	void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
-		if ((key == GLFW_KEY_ESCAPE || key == GLFW_KEY_Q) && action == GLFW_PRESS) {
-			glfwSetWindowShouldClose(window, GLFW_TRUE);
-		}
-	}
-
-	void window_resize_callback(GLFWwindow* window, int width, int height);
-
-	bool get_glfw_vulkan_required_extensions(std::vector<c_str>& result) {
-		uint32_t count; // glfw frees returned arrays
-		const c_str* extensions = glfwGetRequiredInstanceExtensions(&count);
-		if (!extensions) {
-			std::cout << "GLFW doesn't support Vulkan." << std::endl;
-			return false;
-		}
-		result = std::vector<c_str>(extensions, extensions + count);
-		return true;
-	}
+        bool get_glfw_vulkan_required_extensions(std::vector<c_str>& result) {
+                uint32_t count; // glfw frees returned arrays
+                const c_str* extensions = glfwGetRequiredInstanceExtensions(&count);
+                if (!extensions) {
+                        std::cout << "GLFW doesn't support Vulkan." << std::endl;
+                        return false;
+                }
+                result = std::vector<c_str>(extensions, extensions + count);
+                return true;
+        }
 }
 
-
 struct GLFW_vulkan_display : public Window_inteface {
-	bool glfw_initialised = false;
-	GLFWwindow* window = nullptr;
-	Vulkan_display vulkan;
-	
-	std::byte* image;
-	int image_width, image_height;
+        bool glfw_initialised = false;
+        GLFWwindow* window = nullptr;
+        Vulkan_display vulkan;
+        
+        std::byte* image;
+        int image_width, image_height;
 
-	GLFW_vulkan_display() {
-		
-		auto image = stbi_load("./resources/picture.png", &image_width, &image_height, nullptr, 4);
-		assert(image);
-		this->image = reinterpret_cast<std::byte*>(image);
+        GLFW_vulkan_display() {
+                
+                auto image = stbi_load("./resources/picture.png", &image_width, &image_height, nullptr, 4);
+                assert(image);
+                this->image = reinterpret_cast<std::byte*>(image);
 
-		if (!glfwInit()) {
-			throw std::runtime_error{ "GLFW cannot be initialised." };
-		}
-		glfw_initialised = true;
-		glfwSetErrorCallback(glfw_error_callback);
+                if (!glfwInit()) {
+                        throw std::runtime_error{ "GLFW cannot be initialised." };
+                }
+                glfw_initialised = true;
+                glfwSetErrorCallback(glfw_error_callback);
 
-		std::vector<c_str> required_extensions;
-		if (!get_glfw_vulkan_required_extensions(required_extensions)) {
-			throw std::runtime_error{ "Vulkan is not supported by glfw." };
-		}
+                std::vector<c_str> required_extensions;
+                if (!get_glfw_vulkan_required_extensions(required_extensions)) {
+                        throw std::runtime_error{ "Vulkan is not supported by glfw." };
+                }
 
-		vulkan.create_instance(required_extensions);
+                vulkan.create_instance(required_extensions);
 
-		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-		window = glfwCreateWindow(800, 800, "GLFW/Vulkan Window", NULL, NULL);
-		if (!window) {
-			throw std::runtime_error{ "Window cannot be created." };
-		}
+                glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+                window = glfwCreateWindow(800, 800, "GLFW/Vulkan Window", NULL, NULL);
+                if (!window) {
+                        throw std::runtime_error{ "Window cannot be created." };
+                }
 
-		glfwSetWindowUserPointer(window, this);
-		glfwSetKeyCallback(window, key_callback);
-		glfwSetWindowSizeCallback(window, window_resize_callback);
+                glfwSetWindowUserPointer(window, this);
+                glfwSetKeyCallback(window, key_callback);
+                glfwSetWindowSizeCallback(window, window_resize_callback);
 
-		VkSurfaceKHR raw_surface;
-		if (glfwCreateWindowSurface(vulkan.get_instance(), window, NULL, &raw_surface) != VK_SUCCESS) {
-			throw std::runtime_error{ "Vulkan surface cannot be created." };
-		}
+                VkSurfaceKHR raw_surface;
+                if (glfwCreateWindowSurface(vulkan.get_instance(), window, NULL, &raw_surface) != VK_SUCCESS) {
+                        throw std::runtime_error{ "Vulkan surface cannot be created." };
+                }
 
+                vulkan.init(raw_surface, this);
+        }
 
-		vulkan.init(raw_surface, this);
-	}
+        int run() {
+                while (!glfwWindowShouldClose(window)) {
+                        glfwPollEvents();
+                        vulkan.render(image, image_width, image_height);
+                }
 
-	int run() {
-		while (!glfwWindowShouldClose(window)) {
-			glfwPollEvents();
-			vulkan.render(image, image_width, image_height);
-		}
+                return 0;
+        }
 
-		return 0;
-	}
+        ~GLFW_vulkan_display() {
+                if (window) glfwDestroyWindow(window);
+                if (glfw_initialised) glfwTerminate();
+        }
 
-	~GLFW_vulkan_display() {
-		if (window) glfwDestroyWindow(window);
-		if (glfw_initialised) glfwTerminate();
-	}
-
-
-	std::pair<uint32_t, uint32_t> get_window_size() override
-	{
-		int width, height;
-		glfwGetFramebufferSize(window, &width, &height);
-		return {static_cast<uint32_t>(width), static_cast<uint32_t>(height) };
-	}
+        std::pair<uint32_t, uint32_t> get_window_size() override
+        {
+                int width, height;
+                glfwGetFramebufferSize(window, &width, &height);
+                return {static_cast<uint32_t>(width), static_cast<uint32_t>(height) };
+        }
 
 };
 
 namespace {
-	void window_resize_callback(GLFWwindow* window, int width, int height) {
-		auto display = reinterpret_cast<GLFW_vulkan_display*>(glfwGetWindowUserPointer(window));
-		display->vulkan.resize_window();
-	}
+        void window_resize_callback(GLFWwindow* window, int width, int height) {
+                auto display = reinterpret_cast<GLFW_vulkan_display*>(glfwGetWindowUserPointer(window));
+                display->vulkan.resize_window();
+        }
 } // namespace
 
 /*
 int main() {
-	GLFW_vulkan_display display{};
-	return display.run();
+        GLFW_vulkan_display display{};
+        return display.run();
 }*/
 
 //------------------------------------SDL2_WINDOW-------------------------------------------------
@@ -138,131 +133,122 @@ int main() {
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_vulkan.h>
 
-
 class SDL_vulkan_display : Window_inteface{
 
-	bool sdl_initialised = false;
-	Vulkan_display vulkan;
-	SDL_Window* window;
-	bool window_should_close = false;
-	
-	std::byte* image;
-	int image_width, image_height;
-	
+        bool sdl_initialised = false;
+        Vulkan_display vulkan;
+        SDL_Window* window;
+        bool window_should_close = false;
+        
+        std::byte* image;
+        int image_width, image_height;
+        
 
-	struct Color {
-		unsigned char r, g, b;
-	};
-	std::vector<Color> image2;
-	uint32_t image2_width = 2021, image2_height = 999;
-
+        struct Color {
+                unsigned char r, g, b;
+        };
+        std::vector<Color> image2;
+        uint32_t image2_width = 2021, image2_height = 999;
 
 public:
-	SDL_vulkan_display() {
-		image2.resize(image2_height * image2_width, { 0, 0, 255 });
-		for (uint32_t x = 0; x < image2_width; x++) {
-			if (x % 128 < 64) {
-				for (uint32_t y = 0; y < image2_height; y++) {
-					image2[x + size_t{ y } *image2_width] = { 255, 0, 0 };
-				}
-			}
-		}
+        SDL_vulkan_display() {
+                image2.resize(image2_height * image2_width, { 0, 0, 255 });
+                for (uint32_t x = 0; x < image2_width; x++) {
+                        if (x % 128 < 64) {
+                                for (uint32_t y = 0; y < image2_height; y++) {
+                                        image2[x + size_t{ y } *image2_width] = { 255, 0, 0 };
+                                }
+                        }
+                }
 
+                auto image = stbi_load("./resources/picture2.jpg", &image_width, &image_height, nullptr, 4);
+                assert(image);
+                this->image = reinterpret_cast<std::byte*>(image);
 
-		auto image = stbi_load("./resources/picture2.jpg", &image_width, &image_height, nullptr, 4);
-		assert(image);
-		this->image = reinterpret_cast<std::byte*>(image);
+                if (SDL_Init(SDL_INIT_EVENTS) != 0) {
+                        throw std::runtime_error("SDL cannot be initialised.");
+                }
+                sdl_initialised = true;
 
-		if (SDL_Init(SDL_INIT_EVENTS) != 0) {
-			throw std::runtime_error("SDL cannot be initialised.");
-		}
-		sdl_initialised = true;
+                window = SDL_CreateWindow("SDL Vulkan window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                        800, 800, SDL_WINDOW_VULKAN | SDL_WINDOW_RESIZABLE);
+                if (!window) {
+                        std::cout << SDL_GetError() << std::endl;
+                        throw std::runtime_error("SDL window cannot be created.");
+                }
 
-		window = SDL_CreateWindow("SDL Vulkan window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-			800, 800, SDL_WINDOW_VULKAN | SDL_WINDOW_RESIZABLE);
-		if (!window) {
-			std::cout << SDL_GetError() << std::endl;
-			throw std::runtime_error("SDL window cannot be created.");
-		}
+                uint32_t extension_count = 0;
+                SDL_Vulkan_GetInstanceExtensions(window, &extension_count, nullptr);
+                std::vector<const char*> required_extensions(extension_count);
+                SDL_Vulkan_GetInstanceExtensions(window, &extension_count, required_extensions.data());
+                assert(extension_count > 0);
 
+                vulkan.create_instance(required_extensions);
+                auto& instance = vulkan.get_instance();
 
-		uint32_t extension_count = 0;
-		SDL_Vulkan_GetInstanceExtensions(window, &extension_count, nullptr);
-		std::vector<const char*> required_extensions(extension_count);
-		SDL_Vulkan_GetInstanceExtensions(window, &extension_count, required_extensions.data());
-		assert(extension_count > 0);
+                VkSurfaceKHR surface;
+                if (!SDL_Vulkan_CreateSurface(window, instance, &surface)) {
+                        throw std::runtime_error("SDL cannot create surface.");
+                }
 
-		vulkan.create_instance(required_extensions);
-		auto& instance = vulkan.get_instance();
+                vulkan.init(surface, this);
+                
+        }
 
-		VkSurfaceKHR surface;
-		if (!SDL_Vulkan_CreateSurface(window, instance, &surface)) {
-			throw std::runtime_error("SDL cannot create surface.");
-		}
+        ~SDL_vulkan_display() {
+                if (window) SDL_DestroyWindow(window);
+                if (sdl_initialised) SDL_Quit();
+        }
 
-		vulkan.init(surface, this);
-		
-	}
+        std::pair<uint32_t, uint32_t> get_window_size() override
+        {
+                int width, height;
+                SDL_GetWindowSize(window, &width, &height);
+                return { static_cast<uint32_t>(width), static_cast<uint32_t>(height) };
+        }
 
+        int run() {
+                int count = 0;
+                while (!window_should_close) {
+                        SDL_Event event;
+                        while (SDL_PollEvent(&event) != 0) {
+                                switch (event.type) {
+                                        case SDL_EventType::SDL_QUIT:
+                                                window_should_close = true;
+                                                break;
+                                        case SDL_EventType::SDL_WINDOWEVENT:
+                                                if (event.window.event == SDL_WINDOWEVENT_EXPOSED
+                                                        || event.window.event == SDL_WINDOWEVENT_SIZE_CHANGED) 
+                                                {
+                                                        vulkan.resize_window();
+                                                }
+                                                break;
+                                        case SDL_EventType::SDL_KEYDOWN:
+                                                if (event.key.keysym.sym == SDL_KeyCode::SDLK_q) {
+                                                        window_should_close = true;
+                                                        break;
+                                                }
+                                }
+                        }
+                        count++;
+                        if (count < 150) {
+                                vulkan.render(reinterpret_cast<std::byte*>(image2.data()), image2_width, image2_height, 
+                                        sizeof(Color) == 4 ? vk::Format::eR8G8B8A8Srgb : vk::Format::eR8G8B8Srgb);
+                        }
+                        else {
+                                vulkan.render(image, image_width, image_height);
+                        }
+                }
 
-	~SDL_vulkan_display() {
-		if (window) SDL_DestroyWindow(window);
-		if (sdl_initialised) SDL_Quit();
-	}
-
-	std::pair<uint32_t, uint32_t> get_window_size() override
-	{
-		int width, height;
-		SDL_GetWindowSize(window, &width, &height);
-		return { static_cast<uint32_t>(width), static_cast<uint32_t>(height) };
-	}
-
-	int run() {
-		int count = 0;
-		while (!window_should_close) {
-			SDL_Event event;
-			while (SDL_PollEvent(&event) != 0) {
-				switch (event.type) {
-					case SDL_EventType::SDL_QUIT:
-						window_should_close = true;
-						break;
-					case SDL_EventType::SDL_WINDOWEVENT:
-						if (event.window.event == SDL_WINDOWEVENT_EXPOSED
-							|| event.window.event == SDL_WINDOWEVENT_SIZE_CHANGED) 
-						{
-							vulkan.resize_window();
-						}
-						break;
-					case SDL_EventType::SDL_KEYDOWN:
-						if (event.key.keysym.sym == SDL_KeyCode::SDLK_q) {
-							window_should_close = true;
-							break;
-						}
-				}
-			}
-			count++;
-			if (count < 150) {
-				vulkan.render(reinterpret_cast<std::byte*>(image2.data()), image2_width, image2_height, 
-					sizeof(Color) == 4 ? vk::Format::eR8G8B8A8Srgb : vk::Format::eR8G8B8Srgb);
-			}
-			else {
-				vulkan.render(image, image_width, image_height);
-			}
-		}
-
-		return 0;
-	}
+                return 0;
+        }
 };
 
-
 int main() {
-	SDL_vulkan_display display{};
-	display.run();
-	return 0;
+        SDL_vulkan_display display{};
+        display.run();
+        return 0;
 }
-
-
-
 
 
 
